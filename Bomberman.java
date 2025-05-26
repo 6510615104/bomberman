70cc62e1af492883bd347a72e1934e2cf248a0ae
6510615104 <6510615104@student.tu.ac.th>
2025-05-26 22:28:05 +0700

    update

--- Bomberman.java
+++ Bomberman.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class Bomberman {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Bomberman");
-        GameGrid grid = new GameGrid(10, 10, 40);
+        GameGrid grid = new GameGrid(11, 13, 40);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(grid);
         frame.pack();
